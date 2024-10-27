b9d9f6f3e406091760579ca7522e0ca4a625ce0b
Davide Freitas <1221363@isep.ipp.pt>
2024-10-27 23:55:40 +0000

    Sprint 1 final commit final

--- BDDAD/usbd06/usbd06.sql
+++ BDDAD/usbd06/usbd06.sql
@@ -1,7 +1,14 @@
-SELECT wt.name AS WorkstationType
-FROM ProductionOrder po
-    INNER JOIN Product p ON po.Productcode = p.code
-    INNER JOIN BillOfOperations boo ON p.ProductFamily_ID = boo.ProductFamilyID
-    INNER JOIN Operation_WorkstationType owt ON boo.OperationID = owt.Operation_ID
-    INNER JOIN WorkstationType wt ON owt.WorkstationType_ID = wt.ID
-WHERE po.OrderID = 1;
+SELECT DISTINCT
+    wt.name AS WorkstationType
+FROM
+    ProductionOrder po
+    INNER JOIN
+        Product p ON po.Productcode = p.code
+    INNER JOIN
+        BillOfOperations boo ON p.ProductFamily_ID = boo.ProductFamilyID
+    INNER JOIN
+        Operation_WorkstationType owt ON boo.OperationID = owt.Operation_ID
+    INNER JOIN
+        WorkstationType wt ON owt.WorkstationType_ID = wt.ID
+WHERE
+    po.OrderID = 1;
